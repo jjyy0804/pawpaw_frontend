0e6245d952bfa28f593f66a3c35a8c27e8ea7123
jjyy0804 <jjyy0804@naver.com>
2025-04-23 16:33:56 +0900

    ✨ 버튼 컴포넌트 Storybook 문서 작성 및 상태별 예제 추가
    버튼 컴포넌트 Storybook 문서 작성 및 상태별 예제 추가

--- src/stories/Button.stories.tsx
+++ src/stories/Button.stories.tsx
@@ -1,18 +1,37 @@
+import Button from "@/components/Button";
 import type { Meta, StoryObj } from "@storybook/react";
-import Button from "../components/Button";
+
+
 const meta: Meta<typeof Button> = {
-  title: "Common/Button",
+  title: "Components/Button",
   component: Button,
   tags: ["autodocs"],
+  argTypes: {
+    onClick: { action: "clicked!" },
+  },
 };
 
 export default meta;
-
 type Story = StoryObj<typeof Button>;
 
-export const Primary: Story = {
+export const Default: Story = {
   args: {
-    children: "Click Me",
-    containerStyles: "bg-blue-500",
+    children: "기본 버튼",
+    containerStyles: "w-24 h-12 bg-gray-700",
+  },
+};
+
+export const Disabled: Story = {
+  args: {
+    children: "비활성화됨",
+    disabled: true,
+    containerStyles: "w-24 h-12 bg-gray-700",
+  },
+};
+
+export const Loading: Story = {
+  args: {
+    isLoading: true,
+    containerStyles: "w-24 h-12 bg-gray-700",
   },
 };
